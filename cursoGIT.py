fcf22da911eb12b4db7c167fb55543f7450c2a8b
facosta73 <facosta73@hotmail.com>
2021-07-07 08:43:41 -0300

    intento de corregir los guiones

--- cursoGIT.py
+++ cursoGIT.py
@@ -1,11 +1,5 @@
 #check email address. primeras versiones
 
-#de esta versión voy al siguiente archivo OKOK
-#corrijo el doble @
-#con el tipo jara.acosta@gmail.com.ar (aquí fallaría, porque hay 3 puntos, por tanto le digo a contador<4)
-#corrijo el doble punto ".."
-
-
 miemail=input("Introduzca su dirección de email: ")
 
 cuenta_arroba=0
@@ -19,27 +13,33 @@ for i in miemail:
 	if(i=="."):
 		contador_punto=contador_punto+1
 
+contador_guion=0
+for g in miemail:
+	if(g=="_"):
+		contador_guion=contador_guion+1
+
 
 if cuenta_arroba>1:
 	print("email incorrecto")
 
-elif ".." in miemail:
-	print("mal")
-
-elif contador_punto<3 and "@" in miemail and ".com" in miemail:   
+if contador_punto<3 and "@" in miemail and ".com" in miemail and contador_guion<2:   
 		print("el email es correcto")
 
-elif contador_punto<3 and "@" in miemail and ".es" in miemail:
+elif contador_punto<3 and "@" in miemail and ".es" in miemail and contador_guion<2:
 		print("el email es correcto")
 
-elif contador_punto<3 and "@" in miemail and ".org" in miemail:
+elif contador_punto<3 and "@" in miemail and ".org" in miemail and contador_guion<2:
 		print("el email es correcto")
 
-elif contador_punto<3 and "@" in miemail and ".eu" in miemail:
+elif contador_punto<3 and "@" in miemail and ".eu" in miemail and contador_guion<2:
 		print("el email es correcto")
 
-elif contador_punto<4 and "@" in miemail and ".com.ar" in miemail: 
+elif contador_punto<4 and "@" in miemail and ".com.ar" in miemail and contador_guion<2: #jara.acosta@gmail.com.ar (aquí fallaría, porque hay 3 puntos, por tanto le digo a contador<4)
 		print("el email es correcto")
+
+elif cuenta_arroba>1:
+	print("email incorrecto")
+
 
 else:
 	print("email incorrecto")
