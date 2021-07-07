d70d8ba2efc8a4eeb22b30a700f0e2bfdbc80d05
facosta73 <facosta73@hotmail.com>
2021-07-07 08:45:49 -0300

    falla para doble punto

--- cursoGIT.py
+++ cursoGIT.py
@@ -1,5 +1,8 @@
 #check email address. primeras versiones
 
+#ejercicio para emails, por ahora el mejor al 21/06/21. Falla para facosta73@gmail..com
+
+
 miemail=input("Introduzca su dirección de email: ")
 
 cuenta_arroba=0
@@ -13,33 +16,33 @@ for i in miemail:
 	if(i=="."):
 		contador_punto=contador_punto+1
 
-contador_guion=0
-for g in miemail:
-	if(g=="_"):
-		contador_guion=contador_guion+1
+contador_doblepunto=0
+for d in miemail:
+	if(d==".."):
+		contador_doblepunto=contador_doblepunto+1
+
 
 
 if cuenta_arroba>1:
 	print("email incorrecto")
 
-if contador_punto<3 and "@" in miemail and ".com" in miemail and contador_guion<2:   
-		print("el email es correcto")
-
-elif contador_punto<3 and "@" in miemail and ".es" in miemail and contador_guion<2:
-		print("el email es correcto")
-
-elif contador_punto<3 and "@" in miemail and ".org" in miemail and contador_guion<2:
-		print("el email es correcto")
-
-elif contador_punto<3 and "@" in miemail and ".eu" in miemail and contador_guion<2:
-		print("el email es correcto")
-
-elif contador_punto<4 and "@" in miemail and ".com.ar" in miemail and contador_guion<2: #jara.acosta@gmail.com.ar (aquí fallaría, porque hay 3 puntos, por tanto le digo a contador<4)
-		print("el email es correcto")
-
-elif cuenta_arroba>1:
+elif contador_doblepunto>0:
 	print("email incorrecto")
 
+elif contador_punto<3 and "@" in miemail and ".com" in miemail:   #podría probar poniendo un elif tipo elif "..com" in miemail print "incorrecto"
+		print("el email es correcto")
+
+elif contador_punto<3 and "@" in miemail and ".es" in miemail:
+		print("el email es correcto")
+
+elif contador_punto<3 and "@" in miemail and ".org" in miemail:
+		print("el email es correcto")
+
+elif contador_punto<3 and "@" in miemail and ".eu" in miemail:
+		print("el email es correcto")
+
+elif contador_punto<4 and "@" in miemail and ".com.ar" in miemail: 
+		print("el email es correcto")
 
 else:
 	print("email incorrecto")
