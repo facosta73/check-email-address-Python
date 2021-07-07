9fbfd7e7b7f2a62b1d1a8e61a9e6bb3da023f126
facosta73 <facosta73@hotmail.com>
2021-07-07 08:26:03 -0300

    borro primer código

--- cursoGIT.py
+++ cursoGIT.py
@@ -9,25 +9,6 @@ for i in miemail:
 		contador=contador+1
 
 
-if 1<contador<4:
-		print("el email es correcto")
-else:
-	print("email incorrecto")
-
-# no correct code, los siguientes codigos tienen modificaciones
-
-#----------------------------
-#otro intento:
-
-contador=0
-miemail=input("Introduzca su dirección de email: ")
-
-for i in miemail:
-
-	if(i=="@" or i=="."):
-		contador=contador+1
-
-
 if 1<contador<4 and "@" in miemail:
 		print("el email es correcto")
 else:
